091268c4f364cb5a9a5e769332ba50fb31854cf0
ramiro martinez <ramiro.md90@gmail.com>
2026-02-01 12:02:32 -0300

    Avoid empty password hash

--- src/IdentityAccess/Domain/Value/PasswordHash.php
+++ src/IdentityAccess/Domain/Value/PasswordHash.php
@@ -2,12 +2,23 @@
 
 namespace Ramiromd\Sfclean\IdentityAccess\Domain\Value;
 
+use Ramiromd\Sfclean\IdentityAccess\Domain\Exception\InvalidPasswordHash;
+
 class PasswordHash
 {
     private string $value;
 
+    /**
+     * Summary of __construct
+     * @param string $value
+     * @throws InvalidPasswordHash
+     */
     function __construct(string $value)
     {
+        if (empty($value)) {
+            throw new InvalidPasswordHash();
+        }
+
         $this->value = $value;
     }
 
